575ad0778870e7d716f8582663be208019ada1c3
Khanh <hoangcaokhanhhn2004@gmail.com>
2024-11-08 01:03:31 +0100

    configure containers for fitting the image next to it
    Erase parts that are not needed for now

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -10,41 +10,22 @@ const Home: React.FC = () => {
       <section className="hero">
         <div className="hero-content">
           <div className="blur-container">
-            <h1>Xin chào, Cao Khanh is here!!</h1>
+            <h1>About Me</h1>
+            <h2>Software Developer | Typescript | Python | Javascript</h2>
             <p>
-              After two years of studies and living in France, where I am about to complete my Bachelor's in Computer Science at EPITA Paris, I aspire to work in a professional and dynamic environment to maximize my skills. I am particularly interested in Developer roles, where I can leverage my experiences from internships in both France and Vietnam to contribute to sustainable company growth.
+            After two enriching years of studies and experiences in France, I am on the verge of completing my Bachelor's in Computer Science at EPITA Paris, a journey that has deepened my technical expertise and cultural adaptability. My academic path at EPITA has equipped me with a solid foundation in programming languages and technologies, including Flutter, Dart, Node.js, JavaScript, React, Flask, and Python. Alongside technical skills, I have honed essential soft skills like punctuality, teamwork, and time management, which I value greatly and strive to bring into any work environment.
+            </p>
+            <p>
+            My professional experience includes internships in both France and Vietnam, where I worked as a Mobile Software Developer and Web Developer. My recent role at TPBank involved building an Android mobile banking authentication application using Flutter and Dart. This experience developed my skills in API calls, user interface design, and collaboration in a team-oriented setting with regular meetings to align on project goals. Additionally, my internship at Ploutos-Origins in Paris allowed me to work on the company's website using TypeScript and React. Applying Agile methodologies and focusing on testing and debugging, I contributed to a highly functional and user-friendly product, working closely with developers to maintain software quality.
+            </p>
+            <p>
+            Beyond my professional aspirations, I am passionate about football, travel, video games, music, and films, which have enriched my perspective on teamwork and creativity. I am eager to bring my diverse skills and experiences to a Developer role, contributing to a company's sustainable growth while furthering my knowledge and abilities in a professional and dynamic environment. With proficiency in English (C1 level) and an improving grasp of French (A2.2 level), I am confident in my ability to adapt and communicate effectively within multicultural teams.
             </p>
           </div>
         </div>
         <div className="hero-image">
           <img src={logo} alt="Hoang Cao Khanh" />
         </div>
-      </section>
-
-      <section className="about">
-        <div className="centered-blur-container">
-          <h2>About Me</h2>
-        </div>
-        <p>
-          After two years of studies and living in France, where I am about to complete my Bachelor's in Computer Science at EPITA Paris, I aspire to work in a professional and dynamic environment to maximize my skills. I am particularly interested in Developer roles, where I can leverage my experiences from internships in both France and Vietnam to contribute to sustainable company growth.
-        </p>
-      </section>
-
-      <section className="projects">
-        <div className="centered-blur-container">
-          <h2>Professional Experience</h2>
-        </div>
-        <h3>Ploutos-Origins - Paris, France</h3>
-        <p>06/2023 - 08/2023</p>
-        <p>Developed the company website using TypeScript and React. Applied AGILE methodology, collaborated with the team to eliminate software bugs, and implemented software engineering best practices.</p>
-
-        <h3>TPBank - Hanoi, Vietnam</h3>
-        <p>07/2024 - 09/2024</p>
-        <p>Created an Android mobile authentication app with Flutter and Dart. Implemented QR code authentication and worked closely with the team to optimize user experience and functionality.</p>
-
-        <h3>API Gateway Service - Paris, France</h3>
-        <p>04/2024 - 06/2024</p>
-        <p>Developed API gateway services using Django, Python, Node.js, and MongoDB. Worked with team members on GitHub to streamline the workflow and improve service integration.</p>
       </section>
     </div>
   );
